d0f759966724c648c56a95b29177668e6c03e842
Crashdown <vinokurov.ls@phystech.edu>
2020-02-29 20:31:30 +0300

    opa opa

--- src/bot.py
+++ src/bot.py
@@ -58,6 +58,21 @@ def test_message(message):
     bot.send_message(message.chat.id, 'Да что тут тестировать видно же что вы пидор')
 
 
+@bot.message_handler(commands=['monitoring'])
+@bot.message_handler(func=lambda message: message.text.lower() == 'monitoring')
+def monitoring_message(message):
+    if data_base_monitor.check_user(message.chat.id) == 0:
+        bot.send_message(message.chat.id, "Чтобы увидеть свое место в рейтинге, укажите свой e-mail, чтобы мы поняли кто вы")
+        email = get_message()
+        data_base_monitor.reg_user(message.chat.id, email)
+    bot.send_message(message.chat.id, 'Ваши шансы поступить стремятся к размеру вашего члена, сори как бы')
+
+
+@bot.message_handler()
+def get_message(message):
+    return message
+
+
 
 bot_thread = threading.Thread(target=bot.polling)
 bot_thread.start()
