704a460dce55c7a83df700884dbba41fd49ea406
Crashdown <vinokurov.ls@phystech.edu>
2020-02-29 15:30:52 +0300

    plugs for buttons

--- src/bot.py
+++ src/bot.py
@@ -13,4 +13,16 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Hello World', reply_markup=keybord)
 
 
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'faq':
+        bot.send_message(message.chat.id, 'ФАК еще не распирсили, ожидайте')
+    elif message.text.lower() == 'Monitoring':
+        bot.send_message(message.chat.id, 'Ваши шансы поступить стремятся к размеру вашего члена, сори как бы')
+    elif message.text.lower() == 'Test':
+        bot.send_message(message.chat.id, 'Да что тут тестировать видно же что вы пидор')    
+
+
+
+
 bot.polling()
