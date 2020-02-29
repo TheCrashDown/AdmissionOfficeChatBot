d6c011cf7ce5cd17968da6ce029a8e000ab80772
Crashdown <vinokurov.ls@phystech.edu>
2020-02-29 19:59:57 +0300

    monitor to other file

--- src/bot.py
+++ src/bot.py
@@ -8,6 +8,8 @@ import csv
 from config.secret_config.TOKEN import TOKEN
 from config.secret_config.DataBase import DB_NAME, DB_USER, DB_PASSWORD
 
+from monitor_handling import monitoring_message
+
 bot = telebot.TeleBot(TOKEN)
 data_base_telegram = DataBaseTelegram(DB_NAME, DB_USER, DB_PASSWORD)
 data_base_monitor = DataBaseMonitor(DB_NAME, DB_USER, DB_PASSWORD)
@@ -56,14 +58,7 @@ def faq_question(message):
     bot.send_message(message.chat.id, message_to_send, parse_mode="HTML", reply_markup=keybord)
 
 
-@bot.message_handler(commands=['monitoring'])
-@bot.message_handler(func=lambda message: message.text.lower() == 'monitoring')
-def monitoring_message(message):
-    if data_base_monitor.check_user(message.chat.id) == 0:
-        print("Чтобы увидеть свое место в рейтинге, укажите свой e-mail, чтобы мы поняли кто вы")
-        email = input()
-        data_base_monitor.reg_user(message.chat.id, email)
-    bot.send_message(message.chat.id, 'Ваши шансы поступить стремятся к размеру вашего члена, сори как бы')
+
 
 
 @bot.message_handler(commands=['test'])
---
+++ src/monitor_handling.py
@@ -0,0 +1,18 @@
+import telebot
+import threading
+from src.faqer import get_answer
+from src.data_base_telegram import DataBaseTelegram
+from src.data_base_monitor import DataBaseMonitor
+import csv
+
+from config.secret_config.TOKEN import TOKEN
+from config.secret_config.DataBase import DB_NAME, DB_USER, DB_PASSWORD
+
+@bot.message_handler(commands=['monitoring'])
+@bot.message_handler(func=lambda message: message.text.lower() == 'monitoring')
+def monitoring_message(message):
+    if data_base_monitor.check_user(message.chat.id) == 0:
+        print("Чтобы увидеть свое место в рейтинге, укажите свой e-mail, чтобы мы поняли кто вы")
+        email = input()
+        data_base_monitor.reg_user(message.chat.id, email)
+    bot.send_message(message.chat.id, 'Ваши шансы поступить стремятся к размеру вашего члена, сори как бы')
