a4bd28d8a8d8dd5f6ad828473521a836c98fc06d
2ToThe10th <grib.a@phystech.edu>
2020-02-29 18:35:32 +0300

    get/set status add

---
+++ src/data_base_telegram.py
@@ -0,0 +1,58 @@
+import psycopg2
+
+
+class DataBaseTelegram:
+    def __init__(self, db_name, db_user, password):
+        self.data_base = psycopg2.connect(dbname=db_name, user=db_user,
+                                          password=password, host='localhost')
+        self.data_base.autocommit = True
+
+        cursor = self.data_base.cursor()
+
+        cursor.execute("Create table if not exists ABITU ("
+                       "USER_ID varchar(20) primary key, "
+                       "STATUS TEXT DEFAULT NULL, "
+                       "EMAIL varchar(100) DEFAULT NULL"
+                       ");")
+
+        cursor.close()
+
+    def add_user(self, chat_id):
+        cursor = self.data_base.cursor()
+
+        cursor.execute("Select * "
+                       "from abitu "
+                       "where user_id = %(user_id)s",
+                       {'user_id': chat_id})
+
+        if not cursor.row_count:
+            cursor.execute("Insert into abitu (USER_ID, STATUS, EMAIL) "
+                           "values(%(user_id)s, DEFAULT, DEFAULT )",
+                           {'user_id': chat_id})
+
+        cursor.close()
+
+    def get_status(self, chat_id):
+        cursor = self.data_base.cursor()
+
+        cursor.execute("Select STATUS "
+                       "from abitu "
+                       "where user_id = %(user_id)s",
+                       {'user_id': chat_id})
+
+        ret = cursor.fetch
+        cursor.close()
+
+        if ret is None:
+            return None
+        return ret[0]
+
+    def set_status(self, chat_id, status):
+        cursor = self.data_base.cursor()
+
+        cursor.execute("UPDATE abitu "
+                       "set STATUS = %s(status)"
+                       "where user_id = %(user_id)s",
+                       {'user_id': chat_id, 'status': status})
+
+        cursor.close()
