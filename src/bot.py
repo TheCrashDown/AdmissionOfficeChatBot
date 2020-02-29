4cc86e2de8119007d47bb7b3d29b2d2ea49eedcc
Crashdown <vinokurov.ls@phystech.edu>
2020-02-29 14:50:27 +0300

    test bot

--- src/bot.py
+++ src/bot.py
@@ -3,3 +3,15 @@ import telebot
 from config.secret_config.TOKEN import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+
+keybord = telebot.types.ReplyKeyboardMarkup()
+keybord.row("FAQ")
+keybord.row("Test", "Monitoring")
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hello World')
+
+
+if __name__ == '__main__':
+    bot.polling()
