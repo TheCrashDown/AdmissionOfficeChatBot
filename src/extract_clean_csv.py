c0de7b44f6642a7f8c77eec048b495ec1f0dd75a
Igor Maximov <igor@MacBook-Pro-Igor.local>
2020-02-29 17:02:47 +0300

    Clean csv

---
+++ src/extract_clean_csv.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import re
+from parser import parse
+
+def getText(raw_html):
+    cleantextre = re.compile('<.*?>')
+    cleantext = re.sub(cleantextre, '', raw_html)
+    xa0_cleanre = re.compile('\xa0')
+    cleantext = re.sub(xa0_cleanre, ' ', cleantext)
+    return cleantext
+
+def extractCleanCsvQA(src_path, dst_path):
+    df = pd.read_csv(src_path, sep='\t', names=["Question", "Answer"])
+    df["Answer"] = df["Answer"].apply(func=getText)
+    df.to_csv(dst_path, sep='\t')
+
+if __name__ == "__main__":
+    parse()
+    extractCleanCsvQA("data/queries.csv", "~/Desktop/clean_qa.csv")
