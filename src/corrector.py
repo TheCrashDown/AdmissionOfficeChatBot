9ed40d5dab5532c0ba3049b1743f85323742de15
Crashdown <vinokurov.ls@phystech.edu>
2020-02-29 14:04:02 +0300

    add spell corrector and porter

---
+++ src/corrector.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def correct(text):
+
+    url = "https://speller.yandex.net/services/spellservice.json/checkText?text="
+
+    req = url + str.replace(text.strip(), " ", "+")
+
+    resp = requests.get(req).json()
+
+    fixed = text
+    for err in resp:
+        fixed = str.replace(fixed, err["word"], err["s"][0])
+
+    return fixed
+
+if __name__ == '__main__':
+    print(correct("мой дядя самх чесных правилкогда не в шутку занемог"))
