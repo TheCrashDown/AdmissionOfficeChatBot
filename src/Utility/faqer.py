3cfc878b105920aa589392e8d74d33e8170b93b9
gizarchik <galkin.rv@phystech.edu>
2020-03-01 12:21:06 +0300

    fixed import

--- src/Utility/faqer.py
+++ src/Utility/faqer.py
@@ -1,14 +1,13 @@
-import Corrector.corrector as corrector
+from Corrector import correct
 
-
-def delete_noisy_words(text):
+def delete_noisy_words(text, pathname):
     text = text.lower()
 
     import re
     text = " ".join(re.split('[^а-я]', text))
     text = " " + text
 
-    with open("res/noisy_words.txt") as file:
+    with open(pathname) as file:
         noisy_words = [word.replace("\n", "") for word in file.readlines()]
 
     for odd_word in noisy_words:
@@ -19,14 +18,14 @@ def delete_noisy_words(text):
 
 # return number in table
 def get_answer(question):
-    question = corrector.correct(question)
-    question = delete_noisy_words(question)
+    question = correct(question)
+    question = delete_noisy_words(question, "res/noisy_words.txt")
 
-    from Stemmer.porter import Porter as porter
-    question_stemmed_list = [porter.stem(x) for x in question.split(" ") if x]
+    from Stemmer import Porter
+    question_stemmed_list = [Porter.stem(x) for x in question.split(" ") if x]
     print(question_stemmed_list)
 
     print(question)
     pass
 
-# get_answer("На каких правах предоставляется общежитие для поступивших на бюджетной основе?")
+get_answer("На каких правах предоставляется общежитие для поступивших на бюджетной основе?")
