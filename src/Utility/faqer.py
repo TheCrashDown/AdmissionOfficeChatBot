8e844ffc959568d16eb191a32fcc253fef281606
igor <igormaximow@gmail.com>
2020-03-01 11:16:42 +0300

    Package corrections

--- src/Utility/faqer.py
+++ src/Utility/faqer.py
@@ -1,4 +1,4 @@
-import src.corrector as corrector
+import Corrector.corrector as corrector
 
 
 def delete_noisy_words(text):
@@ -22,7 +22,7 @@ def get_answer(question):
     question = corrector.correct(question)
     question = delete_noisy_words(question)
 
-    from src.porter import Porter as porter
+    from Stemmer.porter import Porter as porter
     question_stemmed_list = [porter.stem(x) for x in question.split(" ") if x]
     print(question_stemmed_list)
 
